f2f0a5f2d4a41f4bdae65853ce9383874e4c9c6a
kakaxinow <490422683@qq.com>
2019-02-08 21:54:15 +0800

    2019-2-8 完成集成pacel和对按钮组件的拆分

---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button>按钮2</button>
+</template>
+<script>
+export default {
+   
+}
+</script>
+<style lang="scss">
+ .g-button{
+        padding: 0 1em;
+        font-size: var(--font-size);
+        height: var(--button-height);
+        background-color: var(--button-bg);
+        color: var(--color);
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        &:hover{
+        border-color: var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-bg);
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+    
+</style>
