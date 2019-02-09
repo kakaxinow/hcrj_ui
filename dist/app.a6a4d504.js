0713d04cd38b554f77caaf8f89f26c265eaada94
kakaxinow <490422683@qq.com>
2019-02-09 14:11:36 +0800

    2019-2-8 完成button组件的拆分

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12210,10 +12210,19 @@ exports.default = void 0;
 //
 //
 //
-//
-//
 var _default = {
-  props: ['icon', 'iconPosition']
+  //props:['icon','iconPosition']
+  props: {
+    icon: {},
+    //props第二种写法
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function validator(value) {
+        return !(value !== 'left' && value !== 'right');
+      }
+    }
+  }
 };
 exports.default = _default;
         var $a53c5e = exports.default || module.exports;
@@ -12235,14 +12244,11 @@ exports.default = _default;
       class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
     },
     [
-      _vm.icon
-        ? _c("svg", { staticClass: "icon" }, [
-            _c("use", { attrs: { "xlink:href": "" + _vm.icon } })
-          ])
-        : _vm._e(),
+      _vm.icon ? _c("g-icon", { attrs: { name: _vm.icon } }) : _vm._e(),
       _vm._v(" "),
       _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
-    ]
+    ],
+    1
   )
   var _obj
 }
@@ -12279,6 +12285,71 @@ render._withStripped = true
       
       }
     })();
+},{"_css_loader":"C:/Users/lenovo/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"node_modules/vue-hot-reload-api/dist/index.js","vue":"node_modules/vue/dist/vue.common.js"}],"src/icon.vue":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+//
+//
+//
+//
+//
+var _default = {
+  props: ['name']
+};
+exports.default = _default;
+        var $01cef5 = exports.default || module.exports;
+      
+      if (typeof $01cef5 === 'function') {
+        $01cef5 = $01cef5.options;
+      }
+    
+        /* template */
+        Object.assign($01cef5, (function () {
+          var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("svg", { staticClass: "g-icon" }, [
+    _c("use", { attrs: { "xlink:href": "" + _vm.name } })
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+
+          return {
+            render: render,
+            staticRenderFns: staticRenderFns,
+            _compiled: true,
+            _scopeId: null,
+            functional: undefined
+          };
+        })());
+      
+    /* hot reload */
+    (function () {
+      if (module.hot) {
+        var api = require('vue-hot-reload-api');
+        api.install(require('vue'));
+        if (api.compatible) {
+          module.hot.accept();
+          if (!module.hot.data) {
+            api.createRecord('$01cef5', $01cef5);
+          } else {
+            api.reload('$01cef5', $01cef5);
+          }
+        }
+
+        
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+      }
+    })();
 },{"_css_loader":"C:/Users/lenovo/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"node_modules/vue-hot-reload-api/dist/index.js","vue":"node_modules/vue/dist/vue.common.js"}],"src/app.js":[function(require,module,exports) {
 "use strict";
 
@@ -12286,14 +12357,18 @@ var _vue = _interopRequireDefault(require("vue"));
 
 var _button = _interopRequireDefault(require("./button"));
 
+var _icon = _interopRequireDefault(require("./icon"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _vue.default.component('g-button', _button.default);
 
+_vue.default.component('g-icon', _icon.default);
+
 new _vue.default({
   el: "#app"
 });
-},{"vue":"node_modules/vue/dist/vue.common.js","./button":"src/button.vue"}],"C:/Users/lenovo/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"vue":"node_modules/vue/dist/vue.common.js","./button":"src/button.vue","./icon":"src/icon.vue"}],"C:/Users/lenovo/AppData/Local/Yarn/Data/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -12320,7 +12395,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52751" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54035" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <svg v-if="icon" class="icon">
-            <use :xlink:href = `${icon}`></use>
-        </svg>
+        <g-icon v-if="icon" :name="icon"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -13,9 +11,13 @@ export default {
    //props:['icon','iconPosition']
    props:{
        icon:{},
+       //props第二种写法
        iconPosition:{
            type:String,
-           default:'left'
+           default:'left',
+           validator(value){
+               return !(value!=='left' && value!=='right') 
+           }
        }
    }
 }
