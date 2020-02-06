f959649b764791c487ca24bc085e0ef75663d791
15851641632 <490422683@qq.com>
2020-02-06 10:33:27 +0800

    2020-2-6 10:33 update

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -13086,7 +13086,7 @@ var _default = {
   props: {
     selected: {
       type: String,
-      required: false
+      default: 'sports'
     },
     direction: {
       type: String,
@@ -13619,7 +13619,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61550" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55915" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/tabs.vue
+++ src/tabs.vue
@@ -12,7 +12,7 @@
         props:{
             selected:{
                 type:String,
-                required:false
+                default:'sports'
             },
             direction:{
                 type:String,
